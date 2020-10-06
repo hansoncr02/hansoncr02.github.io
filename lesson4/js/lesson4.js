99eeda3ac79448bd62c11f82ca2fc5a766032575
hansoncr02 <han19037@byui.edu>
2020-10-06 11:44:27 -0600

    css on pages

--- lesson4/js/lesson4.js
+++ lesson4/js/lesson4.js
@@ -6,3 +6,9 @@ window.addEventListener('load', (event)=>{
     copyrightyear.textContent=new Date().getFullYear();
     
 } )
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation');
+
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
