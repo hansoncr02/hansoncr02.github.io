e2ca7fd7b0ebf8df55c1c652a2867e3d9046f93b
hansoncr02 <han19037@byui.edu>
2020-10-12 16:33:06 -0600

    lesson 5 folder

---
+++ lesson5/js/lesson4.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', (event)=>{
+    const lu = document.querySelector('#lastupdated');
+    lu.textContent=document.lastModified;
+
+    const copyrightyear =document.querySelector('#copyrightyear');
+    copyrightyear.textContent=new Date().getFullYear();
+    
+} )
+
+function togglemenu(){
+   document.getElementById("navigation").classList.toggle("responsive");
+
+}
