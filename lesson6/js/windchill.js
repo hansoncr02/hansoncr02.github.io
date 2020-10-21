faff2e138aedccdd420b3c18b84eae5dbe170a76
hansoncr02 <han19037@byui.edu>
2020-10-21 13:30:54 -0600

    windchill works

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,11 +1,16 @@
     
 window.addEventListener('load', (event)=>{
-    var high=document.querySelector('#hightemp');
-    var windspeed=document.querySelector('#wind');
-    console.log(high)
+    var high=document.querySelector('#hightemp').innerHTML;
+    var windspeed=document.querySelector('#wind').innerHTML;
+    console.log(high);
     var windchill= Math.round(35.74+0.6215*high-35.75*Math.pow(windspeed,0.16)+0.4275*high*Math.pow(windspeed,0.16));
     const wc = document.querySelector('#windchill');
-    wc.textContent="Windchill: " + windchill + " \u00B0 F";
+    if (high<50 && windspeed>3.0) {
+        wc.textContent="Windchill: " + windchill + " \u00B0 F";
+    }
+    else {
+        wc.textContent="Windchill: N/A"
+    }
 })
 
     
