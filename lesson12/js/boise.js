dae8ab5740cd24a0d3175fa0684f2cfedd92262f
hansoncr02 <han19037@byui.edu>
2020-12-11 10:05:02 -0700

    javascript started, weather parts accomplished

---
+++ lesson12/js/boise.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', (event)=>{
+    const requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=5586437&units=imperial&APPID=386332280c32fb912ec75c2da387743e';
+    let curtemp;
+        fetch(requestURL)
+            .then((response) => response.json())
+            .then((jsObject) => {console.log(jsObject);
+                curtemp=jsObject.main.temp; 
+               // document.querySelector('#curtemp').textContent = curtemp;
+                console.log(curtemp);
+        });
+        
+});
